cad0576547a91f4f8729759a8e857485d2fab70b
fer <agencydreammaker@gmail.com>
2022-05-24 21:35:56 -0500

    creacion de manager personalizado

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES=(
             ('draft','Draft'),
@@ -13,10 +17,13 @@ class Post(models.Model):
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(max_length=10,choices=STATUS_CHOICES,default='drat')
+    updated=models.DateTimeField(auto_now=True)
+    status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    published=PublishedManager()
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
 
